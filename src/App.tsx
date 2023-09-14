96983f20ce4e51ba58fb73dde00b655f25233148
Ali Taghipour <taghipourali19@gmail.com>
2023-09-14 16:21:55 +0330

    make link shape custom

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,29 @@ import './App.css'
 
 const data = {
   nodes: [
-    { name: 'Node A', startNode: true, size: 20},
-    { name: 'Node B', startNode: true, size: 20 },
-    { name: 'Node C', startNode: true, size: 10},
+    { name: 'T-Mobile', startNode: true, size: 20},
+    { name: 'AT&T', startNode: true, size: 20},
+    { name: 'Verizon', startNode: true, size: 20},
     { name: 'Node D'},
     { name: 'Node E' },
     { name: 'Node F'},
+    { name: 'Node G'},
+    { name: 'Node H' },
+    { name: 'Node I'},
+    { name: 'Node L'},
+    { name: 'Node M' },
+    { name: 'Node N'},
   ],
   links: [
-    { source: 'Node A', target: 'Node B', value: 10 },
-    { source: 'Node B', target: 'Node C', value: 5 },
-    { source: 'Node B', target: 'Node D', value: 8 },
-    { source: 'Node C', target: 'Node E', value: 3 },
-    { source: 'Node D', target: 'Node E', value: 2 },
-    { source: 'Node D', target: 'Node F', value: 4 },
+    { source: 'T-Mobile', target: 'Node D', value: 10 },
+    { source: 'AT&T', target: 'Node E', value: 10 },
+    { source: 'Verizon', target: 'Node F', value: 10 },
+    { source: 'Node D', target: 'Node G', value: 10 },
+    { source: 'Node E', target: 'Node H', value: 10 },
+    { source: 'Node F', target: 'Node I', value: 10 },
+    { source: 'Node G', target: 'Node L', value: 10 },
+    { source: 'Node H', target: 'Node M', value: 10 },
+    { source: 'Node I', target: 'Node N', value: 10 },
   ],
 };
 
@@ -26,8 +35,8 @@ function App() {
 
   return (
     <div>
-      <h1>Sankey Diagram</h1>
-      <SankeyDiagram data={data} width={500} height={300} />
+      <h3>Layer_3_Message_LTE_RRC_Msg</h3>
+      <SankeyDiagram data={data} width={600} height={400}  />
     </div>
   )
 }
--- src/components/SankeyDiagram.tsx
+++ src/components/SankeyDiagram.tsx
@@ -19,8 +19,8 @@ const SankeyDiagram: React.FC<SankeyDiagramProps> = ({ data, width, height }) =>
       const sankeyDiagram = sankey<{}, {}>()
         .nodeId((d:any) => d.name)
         .nodeWidth(20) // Set the custom node width
-        .nodePadding(20) // Set the custom node padding
-        .extent([[0, 0], [width, height]]);
+        .nodePadding(0) // Set the custom node padding
+        .extent([[150, 50], [width, height]]);
       const { nodes, links } = sankeyDiagram(data);
       
       // Create the SVG container
@@ -30,39 +30,47 @@ const SankeyDiagram: React.FC<SankeyDiagramProps> = ({ data, width, height }) =>
 
       // Customize the size of the first node's bar
       const startNodes:any = nodes.filter((node:any) => node.startNode);
-      console.log(startNodes)
       startNodes.forEach(node => {
-        console.log(node)
-        const averageY = (node.y0 + node.y1) / 2;
-        const y0 = averageY - (node.size/2);
-        const y1 = averageY + (node.size/2);
-        node.y0 = y0;
-        node.y1 = y1; 
-        node.sourceLinks.forEach((link,index) => {
-          console.log(link)
-          link.width = node.size;
-          link.y0 = averageY;
-          const targetY0 = link.target.y0;
-          const targetY1 = link.target.y1;
-          link.y1 = (targetY0 + targetY1)/2;
-        })
+        //const averageY = (node.y0 + node.y1) / 2;
+        //const y0 = averageY - (node.size/2);
+       // const y1 = averageY + (node.size/2);
+        //node.y0 = y0;
+        node.y1 = node.y0 + node.size; 
+        // node.sourceLinks.forEach((link,index) => {
+        //   link.width = node.size;
+        //   link.y0 = averageY;
+        //   const targetY0 = link.target.y0;
+        //   const targetY1 = link.target.y1;
+        //   link.y1 = (targetY0 + targetY1)/2;
+        // })
         
       })
       
       // Define a color scale for the nodes
       const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 
+      // Link 
+      const link = sankeyLinkHorizontal();
+
       // Draw the links
       svg.append('g')
         .selectAll('path')
         .data(links)
         .join('path')
-        .attr('d', sankeyLinkHorizontal())
-        .attr('stroke', '#000')
-        .attr('stroke-opacity', 0.2)
-        .attr('fill', 'none')
-        .attr('stroke-width', (d) => Math.max(1, d.width))
-        .style('mix-blend-mode', 'multiply');
+        .attr('d', (d) => {
+         if((d.source as any).startNode){
+          console.log((d.target as any))
+          return `M${(d.source as any).x0 + (d.source as any).size} ${(d.source as any).y0} L${(d.source as any).x1} ${(d.source as any).y1} L${(d.target as any).x0} ${(d.target as any).y1} 
+          L${(d.target as any).x0} ${(d.target as any).y0} Z`;
+         }else{
+          return link(d);
+         }
+        })
+        .attr('stroke', 'lightblue')
+        .attr('stroke-opacity', 0.4)
+        .attr('fill', d => (d.source as any).startNode ? 'red' : 'none')
+        .attr('stroke-width', (d) => Math.max(1,(d.source as any).startNode ? 0 : d.width))
+        .style('mix-blend-mode','multiply');
 
       // Draw the nodes
       svg.append('g')
@@ -81,12 +89,12 @@ const SankeyDiagram: React.FC<SankeyDiagramProps> = ({ data, width, height }) =>
         .selectAll('text')
         .data(nodes)
         .join('text')
-        .attr('x', (d) => (d.x0 ?? 0) + 6)
+        .attr('x', (d) => (d.x0 ?? 0) - 10)
         .attr('y', (d) => (d.y0 ?? 0) + ((d.y1 ?? 0) - (d.y0 ?? 0)) / 2)
         .attr('dy', '0.35em')
-        .attr('text-anchor', 'start')
+        .attr('text-anchor', 'end')
         .text((d:any) => d.name)
-        .attr('fill', '#000')
+        .attr('fill', '#fff')
         .style('font-size', '12px');
     }
   }, [data, width, height]);
